b69eee5508be84d66697690e402d7ec47de850d8
mxdi9i7 <peter.zheng88228@gmail.com>
2020-02-09 17:19:02 -0500

    added track page

--- data/tracks.js
+++ data/tracks.js
@@ -1,34 +1,109 @@
 const tracksData = [
-  {
-    title: "前端工程师",
-    courses: ["se100", "se200", "se400"],
-    timeline: "2个月",
-    description:
-      "使用 HTML、CSS、JavaScript 等专业技能和工具将产品UI设计稿实现成网站产品，涵盖用户PC端、移动端网页，处理视觉和交互问题。",
-    id: "frontend-engineer",
-    thumbnail:
-      "https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255852/FutureSphere/assets/0_th2x89zHuZmHGsLJ.png"
-  },
-  {
-    title: "后端工程师",
-    courses: ["se100", "se200", "se300"],
-    timeline: "2个月",
-    description:
-      "网站的后端涉及搭建服务器、保存和获取数据，以及用于连接前端的接口。后端工程师关心的是如何提升网站的速度、性能和响应性。",
-    id: "backend-engineer",
-    thumbnail:
-      "https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255852/FutureSphere/assets/0_th2x89zHuZmHGsLJ_2.png"
-  },
-  {
-    title: "全栈工程师",
-    courses: ["se100", "se200", "se300", "se400"],
-    timeline: "4个月",
-    description:
-      "全栈工程师的技术涵盖了前端和后端的所有工作，可以作为团队里独当一面的角色。",
-    id: "fullstack-engineer",
-    thumbnail:
-      "https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255553/FutureSphere/assets/fullstack.png"
-  }
+	{
+		title: '前端工程师',
+		courses: ['se100', 'se200', 'se400'],
+		timeline: '2个月',
+		description:
+			'使用 HTML、CSS、JavaScript 等专业技能和工具将产品UI设计稿实现成网站产品，涵盖用户PC端、移动端网页，处理视觉和交互问题。',
+		id: 'frontend-engineer',
+		thumbnail:
+			'https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255852/FutureSphere/assets/0_th2x89zHuZmHGsLJ.png',
+		numbers: {
+			averageSalary: 12500,
+			missingPositions: 170000,
+			timeToTrain: 4,
+			timeToEmployment: 2,
+		},
+		techStacks: ['HTML', 'CSS', 'Javascript'],
+		highlight1: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+		highlight2: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+	},
+	{
+		title: '后端工程师',
+		courses: ['se100', 'se200', 'se300'],
+		timeline: '2个月',
+		description:
+			'网站的后端涉及搭建服务器、保存和获取数据，以及用于连接前端的接口。后端工程师关心的是如何提升网站的速度、性能和响应性。',
+		id: 'backend-engineer',
+		thumbnail:
+			'https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255852/FutureSphere/assets/0_th2x89zHuZmHGsLJ_2.png',
+		numbers: {
+			averageSalary: 12500,
+			missingPositions: 170000,
+			timeToTrain: 4,
+			timeToEmployment: 2,
+		},
+		techStacks: ['HTML', 'CSS', 'Javascript'],
+		highlight1: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+		highlight2: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+	},
+	{
+		title: '全栈工程师',
+		courses: ['se100', 'se200', 'se300', 'se400'],
+		timeline: '4个月',
+		description:
+			'全栈工程师的技术涵盖了前端和后端的所有工作，可以作为团队里独当一面的角色。',
+		id: 'fullstack-engineer',
+		thumbnail:
+			'https://res.cloudinary.com/dtgh01qqo/image/upload/v1578255553/FutureSphere/assets/fullstack.png',
+		numbers: {
+			averageSalary: 12500,
+			missingPositions: 170000,
+			timeToTrain: 4,
+			timeToEmployment: 2,
+		},
+		techStacks: ['HTML', 'CSS', 'Javascript'],
+		highlight1: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+		highlight2: {
+			title: 'lorem ipsum',
+			desc:
+				'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+			image: '',
+		},
+	},
 ];
+
+export const techStacks = {
+	HTML: {
+		title: 'HTML',
+		desc: '',
+		image: '',
+	},
+	CSS: {
+		title: 'CSS',
+		desc: '',
+		image: '',
+	},
+	Javascript: {
+		title: 'Javascript',
+		desc: '',
+		image: '',
+	},
+};
 
 export default tracksData;
